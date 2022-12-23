76a26cce7a3af411e15d127ef2cd79119d26653f
TaysonLe <tayson.l@itcgroup.io>
2022-12-24 05:41:26 +0700

    ui/add decrypt screen

--- main.py
+++ main.py
@@ -1,7 +1,11 @@
-from encrypt.screenEn import encryptScreen
+from encrypt.enScreen import encryptScreen
+from decrypt.deScreen import decryptScreen
 
-en = encryptScreen()
-en.run()
+# en = encryptScreen()
+# en.run()
+
+de = decryptScreen()
+de.run()
 
 
 
